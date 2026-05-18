00d438966998f430ed3031d01b6620635647c054
Aldino Kemal <aldinokemal2104@gmail.com>
2026-05-18 21:18:56 +0700

    chore: bump to PHP 8.2 and update dev dependencies
    - Require PHP ^8.2 (drop 7.4/8.0/8.1 support)
    - Upgrade phpunit to ^10.5 and codeigniter4/framework to ^4.7
    - Mark nullable string parameters in ThemesException for PHP 8.2 compatibility

--- src/Exceptions/ThemesException.php
+++ src/Exceptions/ThemesException.php
@@ -5,17 +5,17 @@ use CodeIgniter\Exceptions\FrameworkException;
 
 class ThemesException extends FrameworkException implements ExceptionInterface
 {
-	public static function forMissingTemplateView(string $template = null)
+	public static function forMissingTemplateView(?string $template = null)
 	{
 		return new static(lang('Themes.missingTemplateView', [$template]));
 	}
 
-	public static function forPluginNotRegistered(string $plugin = null)
+	public static function forPluginNotRegistered(?string $plugin = null)
 	{
 		return new static(lang('Themes.pluginNotRegistered', [$plugin]));
 	}
 
-	public static function forPluginNotFound(string $plugin = null)
+	public static function forPluginNotFound(?string $plugin = null)
 	{
 		return new static(lang('Themes.pluginNotFound', [$plugin]));
 	}
